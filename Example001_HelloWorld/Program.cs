754f220ce87d7ca22279a5a244565a216440faf1
Mahmiev Aslbek <mahmiyev.aslbek@gmail.com>
2023-06-19 10:58:21 +0300

    Example001_Hello_World

---
+++ Example001_HelloWorld/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, ASIL!");
+Console.WriteLine("Hello, ASL!");
+Console.WriteLine("Hello, ASLBEK!");
+Console.WriteLine("Hello, MAN!");
